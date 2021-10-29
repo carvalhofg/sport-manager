86cacd7c377b032ece89edf6112491f4db3496bf
Gabriel Carvalho <carvalho_biel@outlook.com>
2021-10-29 01:07:10 -0300

    Setting project Ok

--- src/test/java/io/gab/sportmanager/SportManagerApplicationTests.java
+++ src/test/java/io/gab/sportmanager/SportManagerApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class SportManagerApplicationTests {
 
 	@Test
-	public void main() {
+	void main() {
 		SportManagerApplication.main(new String[] {});
 	}
 
