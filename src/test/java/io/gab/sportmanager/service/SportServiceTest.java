7bfb52e76515a0c354ab9f613edb01fd82bf2159
Gabriel Carvalho <carvalho_biel@outlook.com>
2022-08-02 06:03:00 -0300

    Adicionado testes unitarios sport service

--- src/test/java/io/gab/sportmanager/service/SportServiceTest.java
+++ src/test/java/io/gab/sportmanager/service/SportServiceTest.java
@@ -10,11 +10,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 class SportServiceTest {
 
@@ -29,12 +31,31 @@ class SportServiceTest {
     }
 
     @Test
-    void getAllSports() {
+    void getOneSport() {
         given(sportRepository.findById(expectedSport.getId())).willReturn(Optional.of(expectedSport));
         SportService sportService = new SportServiceImpl(sportRepository);
         Sport actual = sportService.findById(expectedSport.getId()).get();
         System.out.println(expectedSport);
         Assertions.assertEquals(expectedSport, actual);
+    }
+
+    @Test
+    void getAllSports() {
+        List<Sport> sportList = new ArrayList<>();
+        sportList.add(expectedSport);
+        given(sportRepository.findAll()).willReturn(sportList);
+        SportService sportService = new SportServiceImpl(sportRepository);
+        Assertions.assertEquals(sportService.findAll().size(), 1);
+    }
+
+    @Test
+    void saveSport() {
+        Sport newSport = new Sport();
+        given(sportRepository.save(any(Sport.class))).willReturn(newSport);
+        SportService sportService = new SportServiceImpl(sportRepository);
+        Sport actual = sportService.save(expectedSport);
+        verify(sportRepository, times(1)).save(any(Sport.class));
+        Assertions.assertEquals(actual.getId(), newSport.getId());
     }
 
 }
